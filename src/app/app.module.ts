a8b8b35bc1a886ca2391068107a0f0bd0f5b2e36
feconto <frcontom@poligran.edu.co>
2020-11-17 22:56:44 -0500

    se realiza la subida del proyecto inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { ProductosComponent } from './productos/productos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsuariosComponent,
+    CategoriasComponent,
+    ProductosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
